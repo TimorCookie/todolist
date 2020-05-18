fab20ffe8bdbf81e3fa4f60bab6f5666e30b753e
timokie <xiaoshame@126.com>
2020-05-18 16:28:09 +0800

    todolist with react-redux

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Todo from './Todo'
+import { getTodosByVisibilityFilter } from '../redux/selectors'
+const TodoList = ({todos})=> (
+  <ul className="todo-list">
+    {todos && todos.length 
+      ? todos.map((todo,index)=>{
+        return <Todo todo={todo} key={todo.id}/>
+      })
+      : "no todos yay!"
+    }
+  </ul>
+)
+
+
+const mapStateToProps = state => {
+  const { visibilityFilter} = state
+  const todos = getTodosByVisibilityFilter(state, visibilityFilter)
+  return {todos}
+}
+
+export default connect(mapStateToProps)(TodoList)
